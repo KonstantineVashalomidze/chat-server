ed1d2db5009d442e61e9c4c3dab7f813a35aaea4
Mama africa <k_vashalomidze@mail.ru>
2024-04-29 14:42:22 +0400

    updated reset password

--- Templates/Mail/resetPassword.js
+++ Templates/Mail/resetPassword.js
@@ -134,7 +134,6 @@ module.exports = (name, link) => {
                                     <tr>
                                       <td style="padding-right: 0px;padding-left: 0px;" align="center">
     
-                                        <img align="center" border="0" src="https://assets.unlayer.com/projects/47137/1675000768671-logo.ico" alt="Sateeq Logo" title="Sateeq Logo" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: inline-block !important;border: none;height: auto;float: none;width: 19%;max-width: 106.4px;"
                                           width="106.4" />
     
                                       </td>
@@ -179,8 +178,8 @@ module.exports = (name, link) => {
                                   <div style="color: #ffffff; line-height: 140%; text-align: left; word-wrap: break-word;">
                                     <p style="font-size: 14px; line-height: 140%;"><span style="color: #000000; line-height: 19.6px;">Hello ${name},</span></p>
                                     <p style="font-size: 14px; line-height: 140%;"> </p>
-                                    <p style="font-size: 14px; line-height: 140%;"><span style="color: #000000; line-height: 19.6px;">Please click on the given button to reset your password</span><br /><br /><span style="color: #000000; line-height: 19.6px;">Note: This link is valid for only 10 mins.</span></p>
-                                    <p style="font-size: 14px; line-height: 140%;"><br /><br /><span style="color: #000000; line-height: 19.6px;">Thanks &amp; Regards</span><br /><span style="color: #000000; line-height: 19.6px;">Team Task</span></p>
+                                    <p style="font-size: 14px; line-height: 140%;"><span style="color: #000000; line-height: 19.6px;">Please click on the button to reset your password</span><br /><br /><span style="color: #000000; line-height: 19.6px;">This link is valid for only 5 mins.</span></p>
+                                    <p style="font-size: 14px; line-height: 140%;"><br /><br /><span style="color: #000000; line-height: 19.6px;">Best Regards</span><br /><span style="color: #000000; line-height: 19.6px;"></span></p>
                                   </div>
     
                                 </td>
@@ -263,8 +262,7 @@ module.exports = (name, link) => {
                                 <td style="overflow-wrap:break-word;word-break:break-word;padding:48px;font-family:'Cabin',sans-serif;" align="left">
     
                                   <div style="color: #958d8d; line-height: 170%; text-align: center; word-wrap: break-word;">
-                                    <p style="font-size: 14px; line-height: 170%;">Love 💙 Team Tawk</p>
-                                    <p style="font-size: 14px; line-height: 170%;">For any assistance and queries write to us at <a rel="noopener" href="mailto:support@codingmonk.in?" target="_blank">Support@codingmonk.in</a><br /><br /></p>
+                                    <p style="font-size: 14px; line-height: 170%;">For any questions or concerns email us at <a rel="noopener" href="mailto:giogiodagio3@gmail.com?" target="_blank">giogiodagio3@gmail.com</a><br /><br /></p>
                                   </div>
     
                                 </td>
